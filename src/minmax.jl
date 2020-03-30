91196c8376e942b9fe80f71e698bc287486cea5b
Allen Hill <allenofthehills@gmail.com>
2020-03-30 12:55:42 -0400

    Change includebounds => strictbounds; fix missing/NaN behavior
    The previous behavior of missing/NaN behavior with `includebounds` was not
    logically consistent, and could not be with the original definition of
    `includebounds`. To ensure that the behavior of missing/NaNs was
    logically consistent with bounding behavior, `includebounds` was changed
    to `strictbounds`. `strictbounds` has the same intent, but broader
    application. See updated docstrings for more info.

--- src/minmax.jl
+++ src/minmax.jl
@@ -1,19 +1,19 @@
 export maxima,
        minima
 
-for (funcname, comp) in ((:maxima, :<),
-                         (:minima, :>))
+for (funcname, comp, notcomp) in ((:maxima, :<, :>),
+                         (:minima, :>, :<))
 
     @eval begin
         function ($funcname)(x::AbstractVector,
                         w::Integer=1,
-                        includebounds::Bool=false)
+                        strictbounds::Bool=true)
             w > 0 || throw(ArgumentError("window cannot be negative"))
             xlen = length(x)
             idxs = Int[]
 
             # There can't be more than one peak every `w` elements
-            maxN = includebounds ? cld(xlen,2*w) : fld(xlen,2*w)
+            maxN = strictbounds ? cld(xlen,2*w) : fld(xlen,2*w)
             N = 0
 
             ww = [ collect(-w:-1); collect(1:w) ]
@@ -25,8 +25,8 @@ for (funcname, comp) in ((:maxima, :<),
             i = firsti
             maxI = lasti - w
 
-            if includebounds
-                @inbounds while i <= firsti + w
+            if !strictbounds
+                while i <= firsti + w
                     xi = x[i]
                     peak = true
                     if ismissing(xi)
@@ -37,18 +37,25 @@ for (funcname, comp) in ((:maxima, :<),
                         for j in max(i-w, firsti):(i+w)
                             i === j && continue
                             xj = x[j]
-                            if ismissing(xj) || ($comp)(xi, xj) || isnan(xj)
+                            if coalesce(($comp)(xi, xj), strictbounds) || (strictbounds && (ismissing(xj) || isnan(xj)))
                                 peak &= false
                                 break # No reason to continue checking if the rest of the elements qualify
-                            elseif xi === xj && j > i
+                            elseif xi === xj
                                 k = findnext(y -> xi !== y, x, j+1)
                                 if isnothing(k) # x is constant till the end, not a peak
-                                    peak &= false
-                                    i = lasti+1
-                                    break
+                                    if !strictbounds
+                                        peak &= false
+                                        push!(idxs,i)
+                                        N += 1
+                                        i = lasti+1
+                                        break
+                                    else
+                                        peak &= false
+                                        break
+                                    end
                                 else
                                     xk = x[k]
-                                    if ismissing(xk) || ($comp)(xi, xk) || isnan(xk) # x moves towards a peak
+                                    if coalesce(($comp)(xi, xk), strictbounds) || (strictbounds && (ismissing(xk) || isnan(xk))) # x moves towards a peak
                                         peak &= false
                                         break
                                     else # Push new peak here to shift the right number of elements
@@ -72,9 +79,9 @@ for (funcname, comp) in ((:maxima, :<),
                 end
             end
 
-            i = firsti + w
+            i = max(i, firsti + w, something(findfirst(y -> y !== x[firsti], x), firsti))
 
-            @inbounds while i <= maxI
+            while i <= maxI
                 xi = x[i]
                 peak = true
                 if ismissing(xi)
@@ -84,28 +91,41 @@ for (funcname, comp) in ((:maxima, :<),
                 else
                     for j in ww # For all elements within the window
                         xj = x[i+j]
-                        if ismissing(xj) || ($comp)(xi, xj) || isnan(xj)
+                        if coalesce(($comp)(xi, xj), strictbounds) || (strictbounds && (ismissing(xj) || isnan(xj)))
                             peak &= false
                             break # No reason to continue checking if the rest of the elements qualify
-                        elseif xi === xj && i+j > i
-                            k = findnext(y -> xi !== y, x, i+j+1)
-                            if isnothing(k) # x is constant till the end, not a peak
-                                peak &= false
-                                i = lasti+1
-                                break
-                            else
-                                xk = x[k]
-                                if ismissing(xk) || ($comp)(xi, xk) || isnan(xk)
-                                    # x moves towards a peak or ends with a missing or NaN
-                                    peak &= false
-                                    break
-                                else # Push first element of plateau as peak here to shift the correct number of elements
-                                    peak &= false
-                                    push!(idxs,i)
-                                    N += 1
-                                    i += max(k,i+w)
-                                    break
+                        elseif xi === xj
+                            xi1 = x[i-1]
+                            if coalesce(($notcomp)(xi, xi1), !strictbounds) || (!strictbounds && isnan(xi1))
+                                k = findnext(y -> xi !== y, x, i+j+1)
+                                if isnothing(k)
+                                    if !strictbounds # x is constant till the end, not a peak
+                                        peak &= false
+                                        push!(idxs,i)
+                                        N += 1
+                                        i = lasti+1
+                                        break
+                                    else
+                                        peak &= false
+                                        break
+                                    end
+                                else
+                                    xk = x[k]
+                                    if coalesce(($comp)(xi, xk), strictbounds) || (strictbounds && (ismissing(xk) || isnan(xk)))
+                                        # x moves towards a peak or ends with a missing or NaN
+                                        peak &= false
+                                        break
+                                    elseif j === 1 # Push first element of plateau as peak here to shift the correct number of elements
+                                        peak &= false
+                                        push!(idxs,i)
+                                        N += 1
+                                        i = max(k,i+w)
+                                        break
+                                    end
                                 end
+                            else
+                                peak &= false
+                                break
                             end
                         end
                     end
@@ -119,8 +139,8 @@ for (funcname, comp) in ((:maxima, :<),
                 end
             end
 
-            if includebounds
-                @inbounds while i <= lasti
+            if !strictbounds
+                while i <= lasti
                     xi = x[i]
                     peak = true
                     if ismissing(xi)
@@ -131,27 +151,40 @@ for (funcname, comp) in ((:maxima, :<),
                         for j in (i-w):min((i+w),lasti)
                             i === j && continue
                             xj = x[j]
-                            if ismissing(xj) || ($comp)(xi, xj) || isnan(xj)
+                            if coalesce(($comp)(xi, xj), strictbounds) || (strictbounds && (ismissing(xj) || isnan(xj)))
                                 peak &= false
                                 break # No reason to continue checking if the rest of the elements qualify
-                            elseif xi === xj && j > i
-                                k = findnext(y -> xi !== y, x, j+1)
-                                if isnothing(k) # x is constant till the end, not a peak
-                                    peak &= false
-                                    i = lasti+1
-                                    break
-                                else
-                                    xk = x[k]
-                                    if ismissing(xk) || ($comp)(xi, xk) || isnan(xk) # x moves towards a peak
-                                        peak &= false
-                                        break
-                                    else # Push new peak here to shift the right number of elements
-                                        peak &= false
-                                        push!(idxs,i)
-                                        N += 1
-                                        i = max(k,i+w)
-                                        break
+                            elseif xi === xj
+                                xi1 = x[i-1]
+                                if coalesce(($notcomp)(xi, xi1), !strictbounds) || (!strictbounds && isnan(xi1))
+                                    k = findnext(y -> xi !== y, x, j+1)
+                                    if isnothing(k)
+                                        if !strictbounds # x is constant till the end, not a peak
+                                            peak &= false
+                                            push!(idxs,i)
+                                            N += 1
+                                            i = lasti+1
+                                            break
+                                        else
+                                            peak &= false
+                                            break
+                                        end
+                                    else
+                                        xk = x[k]
+                                        if coalesce(($comp)(xi, xk), strictbounds) || (strictbounds && (ismissing(xk) || isnan(xk))) # x moves towards a peak
+                                            peak &= false
+                                            break
+                                        else # Push new peak here to shift the right number of elements
+                                            peak &= false
+                                            push!(idxs,i)
+                                            N += 1
+                                            i = max(k,i+w)
+                                            break
+                                        end
                                     end
+                                else
+                                    peak &= false
+                                    break
                                 end
                             end
                         end
@@ -175,28 +208,28 @@ for (funcname, comp) in ((:maxima, :<),
 end
 
 @doc """
-    maxima(x[, w=1, includebounds=false])
+    maxima(x[, w=1, strictbounds=true])
 
-Find the local maxima of `x`.
+Find the indices of the local maxima of `x` where each maxima is either the maximum of
+`x[-w:w]` or the first index of a plateau.
 
-`w` sets the minimum allowed distance between maxima. If `includebounds` is `true`, maxima
-are allowed to be less than `w` away from the bounds of `x`, otherwise, maxima may not be
-located any closer to the ends of `x` than `w`. `missing` and `NaN` are effectively treated
-as bounds (ie a peak may not be closer than `w` to a `missing` or `NaN`. The first index of
-a peak that plateaus is used as the peak index.
+If `strictbounds` is `true`, all elements of `x[-w:w]` must exist and may not be `missing`
+or `NaN`. If `strictbounds` is `false`, elements of `x[-w:w]` may not exist (eg peaks may
+be less than `w` indices from either end of `x`), or may be `missing` or `NaN`. `missing`
+or `NaN` must not be maxima.
 """
 maxima
 
 @doc """
-    minima(x[, w=1, includebounds=false])
+    minima(x[, w=1, strictbounds=false])
 
-Find the local minima of `x`.
+Find the indices of the local minima of `x` where each minima is either the minimum of
+`x[-w:w]` or the first index of a plateau.
 
-`w` sets the minimum allowed distance between minima. If `includebounds` is `true`, minima
-are allowed to be less than `w` away from the bounds of `x`, otherwise, minima may not be
-located any closer to the ends of `x` than `w`. `missing` and `NaN` are effectively treated
-as bounds (ie a peak may not be closer than `w` to a `missing` or `NaN`. The first index of
-a peak that plateaus is used as the peak index.
+If `strictbounds` is `true`, all elements of `x[-w:w]` must exist and may not be `missing`
+or `NaN`. If `strictbounds` is `false`, elements of `x[-w:w]` may not exist (eg peaks may
+be less than `w` indices from either end of `x`), or may be `missing` or `NaN`. `missing`
+or `NaN` must not be minima.
 """
 minima
 
--- test/minmax.jl
+++ test/minmax.jl
@@ -16,8 +16,8 @@ x1 = a*sin.(2*pi*f1*T*t)+b*sin.(2*pi*f2*T*t)+c*sin.(2*pi*f3*T*t);
     @test length(maxima(x1)) == 30
     @test length(minima(x1)) == 30
 
-    @test length(maxima(x1, 1, true)) == 31
-    @test length(minima(x1, 1, true)) == 31
+    @test length(maxima(x1, 1, false)) == 31
+    @test length(minima(x1, 1, false)) == 31
 
     # OffsetArrays
     x1_off = OffsetArray(x1, -200:length(x1)-201)
@@ -25,58 +25,109 @@ x1 = a*sin.(2*pi*f1*T*t)+b*sin.(2*pi*f2*T*t)+c*sin.(2*pi*f3*T*t);
     @test length(minima(x1_off)) == 30
     @test maxima(x1_off) == maxima(x1) .- 201
     @test minima(x1_off) == minima(x1) .- 201
-    @test length(maxima(x1_off, 1, true)) == 31
-    @test length(minima(x1_off, 1, true)) == 31
-    @test maxima(x1_off, 1, true) == maxima(x1, 1, true) .- 201
-    @test minima(x1_off, 1, true) == minima(x1, 1, true) .- 201
+    @test length(maxima(x1_off, 1, false)) == 31
+    @test length(minima(x1_off, 1, false)) == 31
+    @test maxima(x1_off, 1, false) == maxima(x1, 1, false) .- 201
+    @test minima(x1_off, 1, false) == minima(x1, 1, false) .- 201
 
     @test length(maxima(x1, 1000)) == 4
     @test length(minima(x1, 1000)) == 4
 
-    @test length(maxima(x1, 1000, true)) == 5
-    @test length(minima(x1, 1000, true)) == 5
+    @test length(maxima(x1, 1000, false)) == 5
+    @test length(minima(x1, 1000, false)) == 5
 
-    # issue #4
-    @test isempty(maxima(zeros(10)))
+    @test maxima([0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],2) == [4,8,12]
+    @test maxima([0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],2,false) == [2,6,10,14]
 
     @testset "Plateaus" begin
-        @test maxima([0,1,1,1,1,0]) == [2]
+        p1 = [0,1,1,1,1,1]
+        p2 = [0,0,1,1,1,1]
+        p3 = [0,0,0,0,1,1]
+
+        @test maxima( p1, 2, false) == [2]
+        @test minima(-p1, 2, false) == [2]
+        @test maxima( p2, 2, false) == [3]
+        @test minima(-p2, 2, false) == [3]
+        @test maxima( p3, 2, false) == [5]
+        @test minima(-p3, 2, false) == [5]
+
+        @test isempty(maxima( p1))
+        @test isempty(minima(-p1))
+        @test isempty(maxima( p2))
+        @test isempty(minima(-p2))
+        @test isempty(maxima( p3))
+        @test isempty(minima(-p3))
+
+        @test maxima(reverse( p1), 2, false) == [1]
+        @test minima(reverse(-p1), 2, false) == [1]
+        @test maxima(reverse( p2), 2, false) == [1]
+        @test minima(reverse(-p2), 2, false) == [1]
+        @test maxima(reverse( p3), 2, false) == [1]
+        @test minima(reverse(-p3), 2, false) == [1]
+
+        @test isempty(maxima(reverse( p1)))
+        @test isempty(minima(reverse(-p1)))
+        @test isempty(maxima(reverse( p2)))
+        @test isempty(minima(reverse(-p2)))
+        @test isempty(maxima(reverse( p3)))
+        @test isempty(minima(reverse(-p3)))
+
+        @test maxima( [0,1,1,1,1,0]) == [2]
         @test minima(-[0,1,1,1,1,0]) == [2]
-
-        @test isempty(maxima([0,1,1,1,1,1]))
-        @test isempty(minima(-[0,1,1,1,1,1]))
-
-        @test maxima([0,1,1,1,2,0]) == [5]
+        @test maxima( [0,1,1,1,2,0]) == [5]
         @test minima(-[0,1,1,1,2,0]) == [5]
+
+        # issue #4
+        @test isempty(maxima(zeros(10)))
+        @test maxima(zeros(10),1,false) == [1]
     end
 
     # A missing or NaN should not occur within the `w` of the peak
     @testset "Missings and NaNs" begin
-        m1 = [0,0,1,1,1,missing,missing,0,1,1,1]
-        n1 = [0,0,1,1,1,NaN,NaN,0,1,1,1]
-        @test isempty(maxima(m1,  1))
-        @test isempty(maxima(n1,  1))
-        @test isempty(minima(-n1, 1))
-        @test isempty(minima(-m1, 1))
+        m1 = [0,0,1,1,1,missing,missing,0,0]
+        n1 = [0,0,1,1,1,NaN,NaN,0,0]
+        @test isempty(maxima( m1, 1, true))
+        @test isempty(maxima( n1, 1, true))
+        @test isempty(minima(-m1, 1, true))
+        @test isempty(minima(-n1, 1, true))
+        @test maxima( m1, 1, false) == [3,8]
+        @test maxima( n1, 1, false) == [3,8]
+        @test minima(-m1, 1, false) == [3,8]
+        @test minima(-n1, 1, false) == [3,8]
+
+        @test isempty(maxima(reverse( m1), 1, true))
+        @test isempty(maxima(reverse( n1), 1, true))
+        @test isempty(minima(reverse(-m1), 1, true))
+        @test isempty(minima(reverse(-n1), 1, true))
+        @test maxima(reverse( m1), 1, false) == [1,5]
+        @test maxima(reverse( n1), 1, false) == [1,5]
+        @test minima(reverse(-m1), 1, false) == [1,5]
+        @test minima(reverse(-n1), 1, false) == [1,5]
 
         m2 = [0,1,2,1,missing,missing,missing]
         n2 = [0,1,2,1,NaN,NaN,NaN]
         @test maxima(m2, 1) == [3]
-        @test isempty(maxima(m2, 2))
         @test maxima(n2, 1) == [3]
+        @test isempty(maxima(m2, 2))
         @test isempty(maxima(n2, 2))
+        @test minima(-m2, 1) == [3]
+        @test minima(-n2, 1) == [3]
+        @test isempty(minima(-m2, 2))
+        @test isempty(minima(-n2, 2))
         @test maxima(reverse(m2), 1) == [5]
-        @test isempty(maxima(reverse(m2), 2))
         @test maxima(reverse(n2), 1) == [5]
+        @test isempty(maxima(reverse(m2), 2))
         @test isempty(maxima(reverse(n2), 2))
+        @test minima(reverse(-m2), 1) == [5]
+        @test minima(reverse(-n2), 1) == [5]
+        @test isempty(minima(reverse(-m2), 2))
+        @test isempty(minima(reverse(-n2), 2))
 
-        @test maxima([m2; 1;2;2;0], 1) == [3,9]
-        @test maxima([n2; 1;2;2;0], 1) == [3,9]
-        @test isempty(maxima([m2; 1;2;2;0], 2))
-        @test isempty(maxima([n2; 1;2;2;0], 2))
-        @test minima(-[m2; 1;2;2;0], 1) == [3;9]
-        @test minima(-[n2; 1;2;2;0], 1) == [3,9]
-        @test isempty(minima(-[m2; 1;2;2;0], 2))
-        @test isempty(minima(-[n2; 1;2;2;0], 2))
+        m3 = [0,1,0,1,missing,1,0,1,0]
+        n3 = [0,1,0,1,NaN,1,0,1,0]
+        @test maxima(m3) == [2,8]
+        @test minima(-m3) == [2,8]
+        @test maxima(n3) == [2,8]
+        @test minima(-n3) == [2,8]
     end
 end
