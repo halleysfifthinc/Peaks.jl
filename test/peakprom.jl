d9af5bc4f88e1666481e2f96c9d1a50a8e9af6e5
Allen Hill <allenofthehills@gmail.com>
2021-07-05 11:41:24 -0400

    Update peakprom tests to avoid deprecated calls

--- test/peakprom.jl
+++ test/peakprom.jl
@@ -14,67 +14,75 @@ x1 = a*sin.(2*pi*f1*T*t)+b*sin.(2*pi*f2*T*t)+c*sin.(2*pi*f3*T*t);
 
 @testset "Peak prominence" begin
     @testset "Reciprocity" begin
-        pi, pp = peakprom(x1)
-        ni, np = peakprom(Minima(), -x1)
-        @test pi == ni
-        @test pp == np
+        maxpks = argmaxima(x1)
+        minpks = argminima(-x1)
+        _, maxprom = peakprom(maxpks, x1)
+        _, minprom = peakprom(minpks, -x1)
+        @test maxprom == minprom
 
-        pi, pp = peakprom(x1; strictbounds=false)
-        ni, np = peakprom(Minima(), -x1; strictbounds=false)
-        @test pi == ni
-        @test pp == np
+        _, maxprom = peakprom(maxpks, x1; strictbounds=false)
+        _, minprom = peakprom(minpks, -x1; strictbounds=false)
+        @test maxprom == minprom
     end
 
     @testset "Prominence values" begin
-        i, p = peakprom(sin.(1e-5:1e-5:9*pi))
+        x2 = sin.(1e-5:1e-5:9*pi)
+        i, p = peakprom(argmaxima(x2), x2)
 
         @test p[[1,5]] ≈ [1., 1.] atol=1e-4
         @test p[[2,3,4]] ≈ [2., 2., 2.] atol=1e-4
 
-        @test last(peakprom([1,0,2,0,1])) == [2]
-        @test last(peakprom([1,0,2,0,1]; strictbounds=false)) == [1,2,1]
+        p2 = [1,0,2,0,1]
+        @test last(peakprom(argmaxima(p2), p2)) == [2]
+        @test last(peakprom(argmaxima(p2; strictbounds=false), p2; strictbounds=false)) == [1,2,1]
 
         # Prominence should be the same regardless of window size
         p1 = [0,0,3,1,2,0,4,0,0,5]
-        @test last(peakprom(p1)) == [3,1,4]
-        @test last(peakprom(p1, 2)) == [3,4]
+        @test last(peakprom(argmaxima(p1, 1), p1)) == [3,1,4]
+        @test last(peakprom(argmaxima(p1, 2), p1)) == [3,4]
 
         # A peaks of the same height count as an intersection for reference intervals
         p4 = [0,4,2,4,3,4,0]
-        @test last(peakprom(p4)) == [2,1,1]
-        @test last(peakprom(p4[2:end-1])) == [1]
-        @test last(peakprom(p4[2:end-1]; strictbounds=false)) == [2,1,1]
+        @test last(peakprom(argmaxima(p4), p4)) == [2,1,1]
+        @test last(peakprom(argmaxima(p4[2:end-1]), p4[2:end-1])) == [1]
+        @test last(peakprom(argmaxima(p4[2:end-1]; strictbounds=false), p4[2:end-1]; strictbounds=false)) == [2,1,1]
 
         # The presence of a missing/NaN in either bounding interval poisons the prominence
         m4 = [missing; p4; missing]
         n4 = [NaN; p4; NaN]
-        @test isequal(last(peakprom(m4)), [missing,1,missing])
-        @test isequal(last(peakprom(n4)), [NaN,1.,NaN])
-        @test last(peakprom(m4; strictbounds=false)) == [2,1,1]
-        @test last(peakprom(n4; strictbounds=false)) == [2.,1.,1.]
+        @test isequal(last(peakprom(argmaxima(m4), m4)), [missing,1,missing])
+        @test isequal(last(peakprom(argmaxima(n4), n4)), [NaN,1.,NaN])
+        @test last(peakprom(argmaxima(m4; strictbounds=false), m4; strictbounds=false)) == [2,1,1]
+        @test last(peakprom(argmaxima(n4; strictbounds=false), n4; strictbounds=false)) == [2.,1.,1.]
 
-        @test last(peakprom([missing,1,missing]; strictbounds=false)) == [0]
-        @test last(peakprom([NaN,1,NaN]; strictbounds=false)) == [0]
+        m5 = [missing, 1, missing]
+        n5 = [NaN, 1, NaN]
+        @test last(peakprom(argmaxima(m5; strictbounds=false), m5; strictbounds=false)) == [0]
+        @test last(peakprom(argmaxima(n5; strictbounds=false), n5; strictbounds=false)) == [0]
 
         p5 = [-1,6,3,4,2,4,2,5,-2,0]
-        @test last(peakprom(p5, 3; strictbounds=false)) == [7,3]
-        @test last(peakprom(reverse(p5), 3; strictbounds=false)) == [3,7]
+        @test last(peakprom(argmaxima(p5, 3; strictbounds=false), p5; strictbounds=false)) == [7,3]
+        @test last(peakprom(argmaxima(reverse(p5), 3; strictbounds=false), reverse(p5); strictbounds=false)) == [3,7]
 
 
     end
 
     @testset "Minimum prominence" begin
         minprom = 1.5
-        i, p = peakprom(sin.(1e-5:1e-5:9*pi))
-        mi, mp = peakprom(sin.(1e-5:1e-5:9*pi); minprom=minprom)
+        x2 = sin.(1e-5:1e-5:9*pi)
+        max2 = argmaxima(x2)
+        _, p = peakprom(max2, x2)
+        _, mp = peakprom(max2, x2; minprom=minprom)
         @test all(x -> x >= minprom, mp)
     end
 
     # issue #4
     let i, p
-        i, p = peakprom(zeros(10))
+        i, p = peakprom(Int[], zeros(10))
         @test isempty(i)
         @test isempty(p)
+        i, p = peakprom([1], zeros(10); strictbounds=false)
+        @test p == [0.0]
     end
 
     @test_deprecated peakprom(Minima(), x1)
