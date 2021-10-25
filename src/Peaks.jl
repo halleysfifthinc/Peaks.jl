8354a1e69005ef27ec39b70daf3a7e5489cfe6b7
Allen Hill <allenofthehills@gmail.com>
2021-10-25 19:32:49 -0400

    Forgot to remove export of deprecated types

--- src/Peaks.jl
+++ src/Peaks.jl
@@ -5,8 +5,6 @@ using Compat
 export argmaxima, argminima, findmaxima, findminima, findnextmaxima, findnextminima,
     peakproms, peakproms!, peakwidths, peakwidths!
 
-export Maxima, Minima
-
 include("minmax.jl")
 include("peakprom.jl")
 include("peakwidth.jl")
