982cb6c3e24fc0d2d58249b01c8bd412bc3c2213
Henning Labuhn <henning.labuhn@depixus.com>
2022-09-19 10:33:27 +0200

    add test for minima

--- test/plotting.jl
+++ test/plotting.jl
@@ -1,15 +1,28 @@
 let
     @testset "Plotting" begin
+        # generate data
         t = 0:1/100:1
         y = 2*sin.(5*t)+3*sin.(10*t)+2*sin.(30*t)
+
+        # find and plot maxima
         pks, vals = findmaxima(y)
         pks, proms = peakproms!(pks, y; minprom=1)
 
         plt = peaksplot(t, y, peaks=pks)
-
         @test plt isa Plots.Plot
 
         savepath_png = abspath(joinpath(@__DIR__, "..", "docs","src","assets","images","peaks_prom_width.png"))
+        savefig(plt, savepath_png)
+        @info "Plots saved to <$savepath_png>"
+
+        # add minima to plot
+        pks, vals = findminima(y)
+        pks, proms = peakproms!(pks, y; minprom=1)
+
+        plt = peaksplot!(t, y, peaks=pks, maxima=false)
+        @test plt isa Plots.Plot
+
+        savepath_png = abspath(joinpath(@__DIR__, "..", "docs","src","assets","images","extrema_prom_width.png"))
         savefig(plt, savepath_png)
         @info "Plots saved to <$savepath_png>"
     end
