c8842b0c7b2e683c8105bde4c7dc80dc460ab4d4
Allen Hill <allenofthehills@gmail.com>
2022-10-18 18:13:02 -0400

    Refactor peaksplot and tweak interface
    (roughly in order of importance)
    
    - Change prominence/width lines to be kwarg flag based
    - For prominence lines only connect horizontal line to minima/maxima
      used for prominence calc
    - Autodetect maxima vs minima
    - Reorder series to change stacking of lines (prefer scatter points to
      be on top)
    - Reuse NaN arrays
    - Tweak visual characteristics of series

--- src/plot.jl
+++ src/plot.jl
@@ -12,75 +12,112 @@ function interp(x::AbstractVector{<:Real}, idx::AbstractVector{<:Real})
     return interp.(Ref(x), idx)
 end
 
+function drop_irrelevant_side(i, peak, y, maxima)
+    if maxima
+        return ifelse(isminima(round(Int, i), y; strict=false), i, peak)
+    else
+        return ifelse(ismaxima(round(Int, i), y; strict=false), i, peak)
+    end
+end
+
 @shorthands peaksplot
-@recipe function f(::Type{Val{:peaksplot}}, x, y, z; peaks::AbstractArray{<:Integer}, maxima=true)
+@recipe function f(::Type{Val{:peaksplot}}, x, y, z; peaks::AbstractVector{Int}, prominences=false, widths=false)
+    if ismaxima(first(peaks), y; strict=false)
+        maxima = true
+    elseif isminima(first(peaks), y; strict=false)
+        maxima = false
+    else
+        throw(error("The first peak in `peaks` is not a local extrema"))
+    end
     sgn = maxima ? -1 : +1
     ext_color = maxima ? :Red : :Green
     ext_label = maxima ? "maxima" : "minima"
 
-    pks, proms = peakproms(peaks, y)
-    vals = y[pks]
-    promlinesx = vec(vcat(reshape(repeat(x[pks]; inner=2), 2, length(pks)), fill(NaN, (1, length(pks)))))
-    promlinesy = vec([vals + sgn * proms vals fill(NaN, (length(pks), 1))]')
-
-    _, widths, lower, upper = peakwidths(pks, y, proms; strict=true)
-    lower_x = interp(x, lower)
-    upper_x = interp(x, upper)
-    halfwidthlinesx = vec([lower_x upper_x fill(NaN, (length(pks), 1))]')
-    halfwidthlinesy = vec(vcat(reshape(repeat(vals + sgn * proms .* 0.5; inner=2), 2, length(pks)), fill(NaN, (1, length(pks)))))
-
-    _, widths, lower, upper = peakwidths(pks, y, proms; strict=true, relheight=prevfloat(1.0))
-    lower_x = interp(x, lower)
-    upper_x = interp(x, upper)
-    fullwidthlinesx = vec([lower_x upper_x fill(NaN, (length(pks), 1))]')
-    fullwidthlinesy = vec(vcat(reshape(repeat(vals + sgn * proms .* 1; inner=2), 2, length(pks)), fill(NaN, (1, length(pks)))))
+    xvals = x[peaks]
+    yvals = y[peaks]
 
     @series begin  # plot raw data
         linewidth --> 2
         seriestype := :path
         label := "signal"
         linecolor --> :Black
-        x, y
+        x := x
+        y := y
     end
-    @series begin  # plot vertical prominence lines
-        seriestype := :path
-        label := "prominence"
-        linewidth := 1
-        linecolor := :Blue
-        x := promlinesx
-        y := promlinesy
+
+    if prominences || widths
+        _, proms = peakproms(peaks, y; strict=false)
     end
-    @series begin  # plot horizontal prominence lines
-        seriestype := :path
-        linewidth := 1
-        label := nothing
-        linecolor := :Blue
-        x := fullwidthlinesx
-        y := fullwidthlinesy
+
+    if prominences
+        nans = fill(NaN, (length(peaks), 1))
+        promlinesx = vec(vcat(reshape(repeat(xvals; inner=2), 2, length(peaks)), nans'))
+        promlinesy = vec([yvals + sgn * proms yvals nans]')
+
+        _, _, lower, upper = peakwidths(peaks, y, proms;
+            strict=false, relheight=prevfloat(1.0))
+
+        lower_x = interp(x, drop_irrelevant_side.(lower, peaks, Ref(y), maxima))
+        upper_x = interp(x, drop_irrelevant_side.(upper, peaks, Ref(y), maxima))
+
+        promwidthlinesx = vec([lower_x upper_x nans]')
+        promwidthlinesy = vec(vcat(
+            reshape(repeat(yvals + sgn * proms; inner=2), 2, length(peaks)),
+            nans'))
+
+        @series begin  # plot vertical prominence lines
+            seriestype := :path
+            label := "prominence"
+            linewidth := 1
+            linecolor := :Blue
+            x := promlinesx
+            y := promlinesy
+        end
+        @series begin  # plot horizontal prominence lines
+            seriestype := :path
+            linewidth := 1
+            label := nothing
+            linecolor := :Blue
+            linealpha := 0.5
+            linestyle := :dash
+            x := promwidthlinesx
+            y := promwidthlinesy
+        end
     end
+
+    if widths
+        nans = fill(NaN, (length(peaks), 1))
+        _, _, lower, upper = peakwidths(peaks, y, proms; strict=false)
+        lower_x = interp(x, lower)
+        upper_x = interp(x, upper)
+
+        halfwidthlinesx = vec([lower_x upper_x nans]')
+        halfwidthlinesy = vec(vcat(reshape(repeat(yvals + sgn * proms .* 0.5; inner=2), 2, length(peaks)), nans'))
+
+        @series begin  # plot width lines
+            seriestype := :path
+            linewidth := 1
+            label := "width"
+            linewidth := 1
+            linecolor := :Gray
+            linestyle := :dash
+            x := halfwidthlinesx
+            y := halfwidthlinesy
+        end
+        @series begin  # plot width points
+            seriestype := :scatter
+            markercolor := :Gray
+            label := nothing
+            x := halfwidthlinesx
+            y := halfwidthlinesy
+        end
+    end
+
     @series begin  # plot extrema points
         seriestype := :scatter
         label := ext_label
         markercolor := ext_color
-        x := x[pks]
-        y := vals
-    end
-    @series begin  # plot width lines
-        seriestype := :path
-        linewidth := 1
-        label := "width"
-        linewidth := 1
-        linecolor := :Gray
-        linestyle := :dot
-        x := halfwidthlinesx
-        y := halfwidthlinesy
-    end
-    @series begin  # plot width points
-        seriestype := :scatter
-        markercolor := :Gray
-        markerstrokecolor := nothing
-        label := nothing
-        x := halfwidthlinesx
-        y := halfwidthlinesy
+        x := xvals
+        y := yvals
     end
 end
