5cb66c34168f38ec28888b8f21463b4bf2579628
KronosTheLate <densbal@live.no>
2023-09-14 19:42:38 +0200

    Remove test code from Peaks.jl

--- src/Peaks.jl
+++ src/Peaks.jl
@@ -14,18 +14,4 @@ include("peakheight.jl")
 include("api_rework.jl")
 include("plot.jl")
 
-function bla(x)
-    pks = findpeaks(x)
-    pks = peakproms!(pks, min=0.5)
-    pks = peakwidths!(pks)
-    pks = peakheights!(pks)
-    return pks
-end
-
-function bla2(x)
-    pks = findpeaks(x) |> peakproms!(min=0.5) |> peakwidths!() |> peakheights!()
-    return pks
-end
-
-export bla, bla2
 end # module Peaks
