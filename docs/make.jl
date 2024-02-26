94e9ceffe24a2a500558dc7cb85a9830e681778b
Allen Hill <allenofthehills@gmail.com>
2024-02-26 13:41:15 -0800

    Update Documenter to v1

--- docs/make.jl
+++ docs/make.jl
@@ -10,13 +10,13 @@ makedocs(
         canonical="https://halleysfifthinc.github.io/Peaks.jl",
     ),
     authors="Allen Hill <allenofthehills@gmail.com> and contributors",
-    repo="https://github.com/halleysfifthinc/Peaks.jl/blob/{commit}{path}#L{line}",
+    repo=Remotes.GitHub("halleysfifthinc", "Peaks.jl")
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/halleysfifthinc/Peaks.jl",
+    repo = Remotes.GitHub("halleysfifthinc", "Peaks.jl"),
     push_preview=true,
 )
