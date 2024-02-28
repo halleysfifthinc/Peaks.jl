5c98dab76f888851f633ee8cea3c58ac14481f3d
Allen Hill <allenofthehills@gmail.com>
2024-02-27 16:59:45 -0800

    Test doctests when running tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,14 @@
 using Peaks
-using Test, OffsetArrays, Plots
+using Test, OffsetArrays, Plots, Documenter
+
+DocMeta.setdocmeta!(Peaks, :DocTestSetup, :(using Peaks); recursive=true)
 
 include("minmax.jl")
 include("peakprom.jl")
 include("peakwidth.jl")
 include("peakheight.jl")
 include("plotting.jl")
+
+@testset "Doctests" begin
+    doctest(Peaks; manual=false)
+end
