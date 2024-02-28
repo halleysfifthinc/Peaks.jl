b2ec0a6b3f23755968e638322c9b915f753e80f8
Allen Hill <allenofthehills@gmail.com>
2024-02-27 18:28:46 -0800

    Dont try linking to Base docs (for the moment)

--- src/minmax.jl
+++ src/minmax.jl
@@ -191,7 +191,7 @@ end
 Find the indices and values of local maxima in `x`, where each maxima `i` is
 either the maximum of `x[i-w:i+w]` or the first index of a plateau.
 
-Returns a [`NamedTuple`](@ref) contains the fields `indices`, `heights`, `data`, which are
+Returns a `NamedTuple` contains the fields `indices`, `heights`, `data`, which are
 equivalent to `heights = data[indices]`. The `data` field is a reference (not a copy) to
 the argument `x`.
 
@@ -329,7 +329,7 @@ end
 Find the indices and values of local minima in `x`, where each minima `i` is
 either the minimum of `x[i-w:i+w]` or the first index of a plateau.
 
-Returns a [`NamedTuple`](@ref) contains the fields `indices`, `heights`, `data`, which are
+Returns a `NamedTuple` contains the fields `indices`, `heights`, `data`, which are
 equivalent to `heights = data[indices]`. The `data` field is a reference (not a copy) to
 the argument `x`.
 
