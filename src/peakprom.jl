b4d6d4f1627753e6e9ca4995ea5c990fe76dea45
Jishnu Bhattacharya <jishnub.github@gmail.com>
2024-03-11 17:31:36 +0530

    Union branches in peakproms! in assigning to proms

--- src/peakprom.jl
+++ src/peakprom.jl
@@ -203,7 +203,17 @@ function peakproms!(peaks::AbstractVector{Int}, x::AbstractVector{T};
                 rref = exm(view(notmval, k:rb)) # Slice corollary upper side
             end
 
-            proms[i] = abs(x[peaks[i]] - exa(coalesce(lref, rref), coalesce(rref, lref)))
+            proms[i] =  abs(x[peaks[i]] -
+                            # exa(coalesce(lref, rref), coalesce(rref, lref)))
+                            # we manually union-split this for better type-inference
+                            if ismissing(lref)
+                                rref
+                            elseif ismissing(rref)
+                                lref
+                            else
+                                exa(lref, rref)
+                            end
+                        )
         end
     end
 
