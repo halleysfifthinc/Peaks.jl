ced33adf8e7ecd34763db74aaa2539298058ef21
Allen Hill <allenofthehills@gmail.com>
2024-08-05 16:07:51 -0700

    Wrap all tests in a testset to improve test summary

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,15 @@ using Test, OffsetArrays, Plots, Documenter
 
 DocMeta.setdocmeta!(Peaks, :DocTestSetup, :(using Peaks); recursive=true)
 
-include("minmax.jl")
-include("peakprom.jl")
-include("peakwidth.jl")
-include("peakheight.jl")
-include("plotting.jl")
-include("utils.jl")
+@testset verbose=true "Peaks" begin
+    include("minmax.jl")
+    include("peakprom.jl")
+    include("peakwidth.jl")
+    include("peakheight.jl")
+    include("plotting.jl")
+    include("utils.jl")
 
-@testset "Doctests" begin
-    doctest(Peaks; manual=false)
+    @testset "Doctests" begin
+        doctest(Peaks; manual=false)
+    end
 end
